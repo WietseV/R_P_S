5ae1ea83a159db588364cb1623a52a6dca10ab22
Orion <wietse.vandeput@hotmail.com>
2023-02-11 18:17:29 +0100

    trying to see what happens when i get wrong score, server side

--- app/gameInfo.tsx
+++ app/gameInfo.tsx
@@ -8,6 +8,7 @@ export default function GameInfo(game: Game){
             <div className="w-full rounded-xl p-8 flex flex-col justify-items-center gap-8 border-2 border-black border-opacity-80">
                 <div className=" flex justify-center items-center">
                     <div className="flex-1 flex flex-col justify-center items-center gap-4 ">
+                        <div>{game.type}</div>
                         <div className="w-full p-4 gradient__orange rounded-lg text-xl text-black font-medium flex justify-between">
                             <h1>Wins: </h1>
                             <h1>{game.wins}</h1>
