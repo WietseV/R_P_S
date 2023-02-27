7c8aa8300e082956d65904d7b510e4b68423ddeb
Orion <wietse.vandeput@hotmail.com>
2023-02-27 15:52:52 +0100

    altered the window catch

--- app/rps/gameInfo.tsx
+++ app/rps/gameInfo.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 export default function GameInfo(){
-    if (window) {return (
+    if ( typeof window !== undefined ) {return (
             <div className="w-full rounded-xl p-8 flex flex-col justify-items-center gap-8 border-2 border-black border-opacity-80">
                 <div className=" flex justify-center items-center">
                     <div className="flex-1 flex flex-col justify-center items-center gap-4 ">
